caaff7ee670490b2e35a7fb83a988d1ff74c236f
Nakorvalole <cwotka@yandex.ru>
2023-03-29 16:52:57 +0300

    module11-task1

--- js/message.js
+++ js/message.js
@@ -2,32 +2,39 @@
 const getTemplate = (selector) => {
   const thumbnailTemplate = document.querySelector(`#${selector}`).content.querySelector(`.${selector}`);
   const thumbnail = thumbnailTemplate.cloneNode(true);
-  const container = document.body;
+  const container = document.querySelector('main');
   container.append(thumbnail);
 
   return container;
 };
 
-const deleteTemplate = (selector) => {
+const addEvent = (selector) => {
   const button = document.querySelector(`.${selector}__button`);
   const successInner = document.querySelector(`.${selector}__inner`);
+
   button.addEventListener('click', () => {
-    document.querySelector(`.${selector}`).classList.add('hidden');
+    document.querySelector(`.${selector}`).remove();
   });
-  if (!successInner.addEventListener('click')) {
-    document.querySelector(`.${selector}`).classList.add('hidden');
-  }
+  successInner.addEventListener('click', () => {
+    document.querySelector(`.${selector}`).remove();
+  });
+};
+
+const deleteTemplate = (selector) => {
+  addEvent(selector);
 };
 
 const showSuccessMessage = (selector) => {
   getTemplate(selector);
   deleteTemplate(selector);
+  addEvent(selector);
 };
 
 
 const showErrorMessage = (selector) => {
   getTemplate(selector);
   deleteTemplate(selector);
+  addEvent(selector);
 };
 
 export { showSuccessMessage, showErrorMessage };
