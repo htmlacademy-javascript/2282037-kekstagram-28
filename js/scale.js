37c054ab622b1ca21c3a1f2941c84288beb6cdd8
Nakorvalole <cwotka@yandex.ru>
2023-04-06 13:19:45 +0300

    module12-task2

--- js/scale.js
+++ js/scale.js
@@ -4,8 +4,8 @@ const SCALE = {
   MAX: 100,
   DEFAULT: 100,
 };
-const radix = 10;
-const divider = 100;
+const RADIX = 10;
+const DIVIDER = 100;
 
 
 const smallerButtonElement = document.querySelector('.scale__control--smaller');
@@ -14,12 +14,12 @@ const scaleInputElement = document.querySelector('.scale__control--value');
 const imageElement = document.querySelector('.img-upload__preview img');
 
 const scaleImage = (value) => {
-  imageElement.style.transform = `scale(${value / divider})`;
+  imageElement.style.transform = `scale(${value / DIVIDER})`;
   scaleInputElement.setAttribute('value', `${value}%`);
 };
 
 const onSmallerButtonClick = () => {
-  const currentValue = parseInt(scaleInputElement.value, radix);
+  const currentValue = parseInt(scaleInputElement.value, RADIX);
   let newValue = currentValue - SCALE.STEP;
   if (newValue < SCALE.MIN) {
     newValue = SCALE.MIN;
@@ -28,7 +28,7 @@ const onSmallerButtonClick = () => {
 };
 
 const onBiggerButtonClick = () => {
-  const currentValue = parseInt(scaleInputElement.value, radix);
+  const currentValue = parseInt(scaleInputElement.value, RADIX);
   let newValue = currentValue + SCALE.STEP;
   if (newValue > SCALE.MAX) {
     newValue = SCALE.MAX;
